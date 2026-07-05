1bc68a8c07ab23304e1cdd7a8cee476af75975e1
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-07-05 23:02:14 +0530

    initial Landing page

--- apps/web/src/components/Landing.tsx
+++ apps/web/src/components/Landing.tsx
@@ -1,19 +1,67 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import Room from "./Room";
 
 const Landing = () => {
   const [name, setName] = useState("");
-  const navigate = useNavigate();
+  const [joined, setJoined] = useState(false);
+  const [localVideoTrack, setLocalVideoTrack] = useState<MediaStreamTrack | undefined>();
+  const [localAudioTrack, setLocalAudioTrack] = useState<MediaStreamTrack | undefined>();
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    const getCam = async () => {
+      try {
+        const stream = await window.navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true
+        });
+
+        const audioTrack = stream.getAudioTracks()[0];
+        const videoTrack = stream.getVideoTracks()[0];
+        setLocalAudioTrack(audioTrack);
+        setLocalVideoTrack(videoTrack);
+        if (videoRef.current) {
+          const stream2 = new MediaStream();
+          stream2.addTrack(videoTrack);
+          videoRef.current.srcObject = new MediaStream([videoTrack]);
+          videoRef.current.play();
+        }
+      } catch (error) {
+        console.error("Error ascending media devices: ", error);
+      }
+    }
+    getCam();
+
+    return () => {
+      localAudioTrack?.stop();
+      localVideoTrack?.stop();
+    }
+  }, []);
   const handleClick = () => {
-    navigate(`/rooms/?name=${name}`)
+    if (!name.trim()) {
+      alert("Please enter a name first: ");
+      return;
+    }
+    setJoined(true);
   }
 
-  return (
-    <>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
-      <button onClick={handleClick}>Join</button>
-    </>
-  )
+  if (!joined) {
+    return (
+      <>
+        <video autoPlay ref={videoRef} width={400} height={400} style={{ transform: 'scaleX(-1)' }}></video>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <button onClick={handleClick}>Join</button>
+      </>
+    )
+  }
+  if (localAudioTrack && localVideoTrack) {
+    return (
+      <>
+        <Room name={name} localAudioTrack={localAudioTrack} localVideoTrack={localVideoTrack} />
+      </>
+    )
+  }
 }
 
 export default Landing;
