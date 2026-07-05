11536780fbd251a1bd4699a4812dd81ca7c8b362
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-07-05 23:01:57 +0530

    remove strict mode and use of reactRouterDom while developing

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,16 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
-import Room from './components/Room';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/rooms" element={<Room />} />
-        </Routes>
-      </BrowserRouter>
+    <Landing />
     </>
   )
 }
